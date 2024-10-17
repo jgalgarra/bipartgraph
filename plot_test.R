4d200a1ec9ea7d70e8d8615494fd7ca15075f839
jgalgarra <javier.algarra@u-tad.com>
2024-10-17 16:56:32 +0200

    plot_test.R deprecated plot_ziggurat syntax call is now fixed

--- plot_test.R
+++ plot_test.R
@@ -10,7 +10,7 @@ network_test <- rbind(network_test,data.frame(GuildB= "B4",A1=1,A2=0,A3=0,A4=0))
 network_test <- rbind(network_test,data.frame(GuildB= "B5",A1=0,A2=0,A3=0,A4=1))
 write.table(network_test,"ntest.csv",sep=",",row.names = FALSE)
 ziggurat_graph("","ntest.csv",square_nodes_size_scale = 3, 
-               displace_legend = c(-0.5,0.3), root_specialist_expand = c(0.5,1))
+                root_specialist_expand = c(0.5,1))
 
 while (!is.null(dev.list())) Sys.sleep(1)
 file.remove("ntest.csv")
