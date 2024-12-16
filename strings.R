cc8d23efcc848386d6b76d5d5da7f1a7a0881eec
jgalgarra <javier.algarra@u-tad.com>
2024-12-16 12:57:09 +0100

    strings parsing bug for texts encoded as LATIN-1

--- strings.R
+++ strings.R
@@ -7,11 +7,13 @@
 
 LocalizedStrings<-function(locale="es") {
   # crea el objeto
-  this<-list(locale=locale, data=read.csv("resources/strings.csv", header=TRUE, row.names=1,  colClasses="character"))
+  this<-list(locale=locale, data=read.csv("resources/strings.csv", header=TRUE, row.names=1,  colClasses="character", encoding="ISO-8859-1"))
   
   # obtiene el texto para la clave indicada
   this$value<-function(keys) {
-    trim <- function (x) gsub("^\\s+|\\s+$", "", x)
+    trim <- function (x) {
+      gsub("^\\s+|\\s+$", "", iconv(x, from = "ISO-8859-1", to = "UTF-8"))
+    }
     val<-this$data[keys, this$locale]
     val[is.na(val)]<-"(error: undefined key)"
     val[is.null(val)]<-"(error: undefined language)"
